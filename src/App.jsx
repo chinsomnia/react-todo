bb66e9ac5d6790ed106868c4033d332eb8c483be
Jachin Antalan <152952438+chinsomnia@users.noreply.github.com>
2025-01-12 00:28:27 -0800

    Add fetch data from Airtable

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,64 @@ import TodoListItem from "./TodoListItem";
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async(todo) => {
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`
+      }
+    }
+
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`
+
+    try {
+      // const airtableData = {
+      //   fields: {
+      //     title: todo,
+      //   },
+      // };
+  
+      const response = await fetch(url, options);
+  
+      if (!response.ok) {      
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      const todos = data.records.map((todo) => {
+        const existingTodo = {
+          title: todo.fields.title,
+          id: todo.id,
+        }
+        return existingTodo;
+      })
+
+      setTodoList(todos)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error.message);
+      return null;
+    }
+  }
   
   useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const savedTodoList = localStorage.getItem("savedTodoList");
-        const initialList = savedTodoList ? JSON.parse(savedTodoList) : [];
-        resolve({
-          data: {
-            todoList: initialList,
-          },
-        });
-      }, 2000);
-    }).then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    });
+    fetchData()
+    // new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     const savedTodoList = localStorage.getItem("savedTodoList");
+    //     const initialList = savedTodoList ? JSON.parse(savedTodoList) : [];
+    //     resolve({
+    //       data: {
+    //         todoList: initialList,
+    //       },
+    //     });
+    //   }, 2000);
+    // }).then((result) => {
+    //   setTodoList(result.data.todoList);
+    //   setIsLoading(false);
+    // });
   }, []);
 
   useEffect(() => {
